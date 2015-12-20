b52ae27f6f71e45b90ab3c486e5a3e0de693c28c
YvesCR <yves.crutain@gmail.com>
2015-12-20 19:11:05 +0000

    disqus issue
    Modified width of disqus comments.

--- launch.r
+++ launch.r
@@ -4,6 +4,6 @@ library(servr)
 getwd()
 
 jekyll(dir = ".", input = "_source", output = "_posts", script = c("Makefile", "build.R")
-       , command = "jekyll build")
+       , command = "jekyll build --destination blog")
 
 
